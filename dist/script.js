8545722f06e7e87873d080c64c595338fd61e33e
dean <dean@beamdot3d.com>
2025-08-18 12:32:21 -0600

    testspin

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,23 @@
 (() => {
   // script.js
   var lottie1 = document.querySelector(".lottie-wrapper.first");
+  var lottieSpin = document.querySelector(".lottie-wrapper.spin");
   var lottie2 = document.querySelector(".lottie-wrapper.second");
   var lottie3 = document.querySelector(".lottie-wrapper.third");
   var button1 = document.querySelector(".button-lottie.first");
+  var buttonSpin = document.querySelector(".button-lottie.spin");
   var button2 = document.querySelector(".button-lottie.second");
   var button3 = document.querySelector(".button-lottie.third");
   var buttonTest = document.querySelector(".button-lottie.test");
+  var blackout = document.querySelector(".blackout");
   button1.addEventListener("click", function(e) {
     lottie1.classList.add("active");
+    lottieSpin.style.opacity = "0";
+    lottie2.classList.remove("active");
+    lottie3.classList.remove("active");
+  });
+  buttonSpin.addEventListener("click", function(e) {
+    lottieSpin.style.opacity = "100";
     lottie2.classList.remove("active");
     lottie3.classList.remove("active");
   });
@@ -18,6 +27,7 @@
     lottie3.classList.remove("active");
   });
   button3.addEventListener("click", function(e) {
+    console.log("roblox pvp bedwars");
     lottie1.classList.remove("active");
     lottie2.classList.remove("active");
     lottie3.classList.add("active");
--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 const lottie1 = document.querySelector(".lottie-wrapper.first");
-// const lottie1B = document.querySelector(".lottie-wrapper.first-b");
+const lottieSpin = document.querySelector(".lottie-wrapper.spin");
 const lottie2 = document.querySelector(".lottie-wrapper.second");
 const lottie3 = document.querySelector(".lottie-wrapper.third");
+
 const button1 = document.querySelector(".button-lottie.first");
+const buttonSpin = document.querySelector(".button-lottie.spin");
+
 const button2 = document.querySelector(".button-lottie.second");
 const button3 = document.querySelector(".button-lottie.third");
+
 const buttonTest = document.querySelector(".button-lottie.test");
 
+const blackout = document.querySelector(".blackout");
+
 button1.addEventListener("click", function (e) {
+  // blackout.style.opacity = "100";
   lottie1.classList.add("active");
+  lottieSpin.style.opacity = "0";
   lottie2.classList.remove("active");
   lottie3.classList.remove("active");
 });
+buttonSpin.addEventListener("click", function (e) {
+  // blackout.style.opacity = "100";
+
+  // lottie1.style.opacity = "0";
+  lottieSpin.style.opacity = "100";
+  lottie2.classList.remove("active");
+  lottie3.classList.remove("active");
+});
+
 button2.addEventListener("click", function (e) {
   lottie1.classList.remove("active");
   lottie2.classList.add("active");
   lottie3.classList.remove("active");
 });
 button3.addEventListener("click", function (e) {
+  console.log("roblox pvp bedwars");
   lottie1.classList.remove("active");
   lottie2.classList.remove("active");
   lottie3.classList.add("active");
