5f9a0ccba321ede975ae4a1e7b4226092c8901eb
dean <dean@beamdot3d.com>
2025-08-21 20:09:16 -0600

    mvp

--- dist/script.js
+++ dist/script.js
@@ -1,45 +1,60 @@
+"use strict";
 (() => {
   // script.js
+  var lottieCTRLSpin = document.querySelector(".lottie-ctrl.spin");
+  var lottieCTRLLights = document.querySelector(".lottie-ctrl.lights");
+  var lottieCTRLVanish = document.querySelector(".lottie-ctrl.vanish");
+  var allLotties = document.querySelectorAll(".lottie-wrapper");
+  var allLottieContents = document.querySelectorAll(".lottie-content-wrapper");
   var lottie1 = document.querySelector(".lottie-wrapper.first");
   var lottieSpin = document.querySelector(".lottie-wrapper.spin");
   var lottie2 = document.querySelector(".lottie-wrapper.second");
   var lottie3 = document.querySelector(".lottie-wrapper.third");
+  var lottieThree = document.querySelector(".lottie-div.third");
+  var btnLottie = document.querySelector(".button-lottie");
+  var btn1 = document.querySelector(".btn-lottie.first");
+  var btn2 = document.querySelector(".btn-lottie.second");
+  var btn3 = document.querySelector(".btn-lottie.third");
   var button1 = document.querySelector(".button-lottie.first");
-  var buttonSpin = document.querySelector(".button-lottie.spin");
   var button2 = document.querySelector(".button-lottie.second");
   var button3 = document.querySelector(".button-lottie.third");
-  var buttonTest = document.querySelector(".button-lottie.test");
-  var blackout = document.querySelector(".blackout");
-  var spinFlag = false;
-  button1.addEventListener("click", function(e) {
-    lottie1.classList.add("active");
-    spinFlag = false;
-    lottieSpin.style.opacity = "0";
-    lottie2.classList.remove("active");
-    lottie3.classList.remove("active");
+  var btnSpin = document.querySelector(".btn-lottie.spin");
+  var btnLights = document.querySelector(".btn-lottie.lights");
+  var btnVanish = document.querySelector(".btn-lottie.vanish");
+  var buttonContainer = document.querySelector(".btn-container");
+  var lottieFlag;
+  var mutationObserver = new MutationObserver((entries) => {
+    console.log("now");
+    const target = entries[entries.length - 1].target.classList[1];
+    document.querySelector(`.btn-lottie.${target}`).click();
+    buttonContainer.style["pointer-events"] = "auto";
   });
-  buttonSpin.addEventListener("click", function(e) {
-    spinFlag = !spinFlag;
-    console.log(spinFlag);
-    if (spinFlag) {
-      lottieSpin.style.opacity = "100";
-      lottie2.classList.remove("active");
-      lottie3.classList.remove("active");
-    } else {
-      lottieSpin.style.opacity = "0";
-    }
+  mutationObserver.observe(lottieCTRLSpin, { attributes: true });
+  mutationObserver.observe(lottieCTRLLights, { attributes: true });
+  mutationObserver.observe(lottieCTRLVanish, { attributes: true });
+  buttonContainer.addEventListener("click", function(e) {
+    const clicked = e.target.closest(".button-lottie");
+    if (!clicked) return;
+    lottieFlag = clicked.classList[1];
+    SetActiveLottie(`${lottieFlag}`);
+    document.querySelector(`.btn-lottie.${lottieFlag}`).click();
   });
-  button2.addEventListener("click", function(e) {
-    lottie1.classList.remove("active");
-    lottie2.classList.add("active");
-    lottie3.classList.remove("active");
-  });
-  button3.addEventListener("click", function(e) {
-    lottie1.classList.remove("active");
-    lottie2.classList.remove("active");
-    lottie3.classList.add("active");
-  });
-  buttonTest.addEventListener("click", function(e) {
-    console.log("test clicked!");
-  });
+  var SetActiveLottie = function(activeLottie) {
+    buttonContainer.style["pointer-events"] = "none";
+    allLotties.forEach((el) => {
+      el.classList.remove("active");
+      el.style.opacity = "0";
+      if (el.classList.contains(activeLottie)) {
+        el.classList.add("active");
+        el.style.opacity = "100";
+      }
+      allLottieContents.forEach((el2) => {
+        el2.classList.remove("active");
+        el2.style.opacity = "0";
+        if (el2.classList.contains(activeLottie)) {
+          el2.classList.add("active");
+        }
+      });
+    });
+  };
 })();
--- script.js
+++ script.js
@@ -1,54 +1,65 @@
+"use strict";
+
+const lottieCTRLSpin = document.querySelector(".lottie-ctrl.spin");
+const lottieCTRLLights = document.querySelector(".lottie-ctrl.lights");
+const lottieCTRLVanish = document.querySelector(".lottie-ctrl.vanish");
+const allLotties = document.querySelectorAll(".lottie-wrapper");
+const allLottieContents = document.querySelectorAll(".lottie-content-wrapper");
 const lottie1 = document.querySelector(".lottie-wrapper.first");
 const lottieSpin = document.querySelector(".lottie-wrapper.spin");
 const lottie2 = document.querySelector(".lottie-wrapper.second");
 const lottie3 = document.querySelector(".lottie-wrapper.third");
-
+const lottieThree = document.querySelector(".lottie-div.third");
+const btnLottie = document.querySelector(".button-lottie");
+const btn1 = document.querySelector(".btn-lottie.first");
+const btn2 = document.querySelector(".btn-lottie.second");
+const btn3 = document.querySelector(".btn-lottie.third");
 const button1 = document.querySelector(".button-lottie.first");
-const buttonSpin = document.querySelector(".button-lottie.spin");
-
 const button2 = document.querySelector(".button-lottie.second");
 const button3 = document.querySelector(".button-lottie.third");
+const btnSpin = document.querySelector(".btn-lottie.spin");
+const btnLights = document.querySelector(".btn-lottie.lights");
+const btnVanish = document.querySelector(".btn-lottie.vanish");
+const buttonContainer = document.querySelector(".btn-container");
+let lottieFlag;
+//...........................................................
 
-const buttonTest = document.querySelector(".button-lottie.test");
-
-const blackout = document.querySelector(".blackout");
-
-let spinFlag = false;
-
-button1.addEventListener("click", function (e) {
-  // blackout.style.opacity = "100";
-  lottie1.classList.add("active");
-  spinFlag = false;
-  lottieSpin.style.opacity = "0";
-  lottie2.classList.remove("active");
-  lottie3.classList.remove("active");
-});
-buttonSpin.addEventListener("click", function (e) {
-  // blackout.style.opacity = "100";
-
-  // lottie1.style.opacity = "0";
-  spinFlag = !spinFlag;
-  console.log(spinFlag);
-  if (spinFlag) {
-    lottieSpin.style.opacity = "100";
-    lottie2.classList.remove("active");
-    lottie3.classList.remove("active");
-  } else {
-    lottieSpin.style.opacity = "0";
-  }
+const mutationObserver = new MutationObserver((entries) => {
+  console.log("now");
+  const target = entries[entries.length - 1].target.classList[1];
+  document.querySelector(`.btn-lottie.${target}`).click();
+  buttonContainer.style["pointer-events"] = "auto";
 });
 
-button2.addEventListener("click", function (e) {
-  lottie1.classList.remove("active");
-  lottie2.classList.add("active");
-  lottie3.classList.remove("active");
-});
-button3.addEventListener("click", function (e) {
-  lottie1.classList.remove("active");
-  lottie2.classList.remove("active");
-  lottie3.classList.add("active");
-});
+mutationObserver.observe(lottieCTRLSpin, { attributes: true });
+mutationObserver.observe(lottieCTRLLights, { attributes: true });
+mutationObserver.observe(lottieCTRLVanish, { attributes: true });
 
-buttonTest.addEventListener("click", function (e) {
-  console.log("test clicked!");
+buttonContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".button-lottie");
+  if (!clicked) return;
+  lottieFlag = clicked.classList[1];
+  SetActiveLottie(`${lottieFlag}`);
+  document.querySelector(`.btn-lottie.${lottieFlag}`).click();
 });
+//...........................................................
+const SetActiveLottie = function (activeLottie) {
+  buttonContainer.style["pointer-events"] = "none";
+  allLotties.forEach((el) => {
+    el.classList.remove("active");
+    el.style.opacity = "0";
+    if (el.classList.contains(activeLottie)) {
+      el.classList.add("active");
+      el.style.opacity = "100";
+    }
+    allLottieContents.forEach((el) => {
+      el.classList.remove("active");
+      el.style.opacity = "0";
+      if (el.classList.contains(activeLottie)) {
+        el.classList.add("active");
+      }
+    });
+  });
+};
+//...........................................................
+//...........................................................
