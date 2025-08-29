6d07220a2b688fc531cdbdf75e3f515e0d65f4a6
dean <dean@beamdot3d.com>
2025-08-29 13:06:19 -0600

    vid end loops

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,27 @@
 (() => {
   // script.js
+  var lottieCTRLSpin = document.querySelector(".lottie-ctrl.spin");
+  var lottieCTRLLights = document.querySelector(".lottie-ctrl.lights");
+  var lottieCTRLVanish = document.querySelector(".lottie-ctrl.vanish");
+  var allLotties = document.querySelectorAll(".lottie-wrapper");
+  var allLottieContents = document.querySelectorAll(".lottie-content-wrapper");
+  var lottie1 = document.querySelector(".lottie-wrapper.first");
+  var lottieSpin = document.querySelector(".lottie-wrapper.spin");
+  var lottie2 = document.querySelector(".lottie-wrapper.second");
+  var lottie3 = document.querySelector(".lottie-wrapper.third");
+  var lottieThree = document.querySelector(".lottie-div.third");
+  var btnLottie = document.querySelector(".button-lottie");
+  var btn1 = document.querySelector(".btn-lottie.first");
+  var btn2 = document.querySelector(".btn-lottie.second");
+  var btn3 = document.querySelector(".btn-lottie.third");
+  var button1 = document.querySelector(".button-lottie.first");
+  var button2 = document.querySelector(".button-lottie.second");
+  var button3 = document.querySelector(".button-lottie.third");
+  var btnSpin = document.querySelector(".btn-lottie.spin");
+  var btnLights = document.querySelector(".btn-lottie.lights");
+  var btnVanish = document.querySelector(".btn-lottie.vanish");
+  var buttonContainer = document.querySelector(".btn-container");
+  var lottieFlag;
   var vidRotation = document.querySelector(".vid-rotation");
   var vidAllWrapFirst = document.querySelector(".vid-all-wrapper.first");
   var vidContentWrapFirst = document.querySelector(
@@ -11,45 +33,111 @@
   var vidContentWrapThird = document.querySelector(
     ".vid-content-wrapper.third"
   );
-  var vidView1 = document.querySelector(".vid-first");
-  var vidView2 = document.querySelector(".vid-second");
-  var vidView3 = document.querySelector(".vid-third");
-  var allVidWrappers = document.querySelectorAll(".vid-all-wrapper");
+  var vidView1 = document.querySelector(".vid.first");
+  var vidView2 = document.querySelector(".vid.second");
+  var vidView3 = document.querySelector(".vid.third");
+  var allVidAllWrappers = document.querySelectorAll(".vid-all-wrapper");
   var allContentWrappers = document.querySelectorAll(".vid-content-wrapper");
+  var allVidWrappers = document.querySelectorAll(".vid-wrapper");
   var allVids = document.querySelectorAll(".vid");
+  var allVidEndWrappers = document.querySelectorAll(".vid-end-wrapper");
   var vidBtnContainer = document.querySelector(".btn-vid-container");
   var vidFlag;
-  console.log("Aug 25, 2025-3:49");
+  var newTimer;
   vidBtnContainer.addEventListener("click", function(e) {
     const clicked = e.target.closest(".button-vid");
     if (!clicked) return;
     vidFlag = clicked.classList[1];
-    console.log(vidFlag);
     setActiveVidAndPlay(vidFlag);
   });
   vidView1.addEventListener("ended", function() {
-    console.log("video 1 has ended");
     vidContentWrapFirst.classList.add("active");
+    setActiveEndVidAndPlay(vidFlag);
+    ReplayIntroVid();
   });
   vidView2.addEventListener("ended", function() {
-    console.log("video 2 has ended");
     vidContentWrapSecond.classList.add("active");
+    setActiveEndVidAndPlay(vidFlag);
+    ReplayIntroVid();
   });
   vidView3.addEventListener("ended", function() {
-    console.log("video 3 has ended");
     vidContentWrapThird.classList.add("active");
+    setActiveEndVidAndPlay(vidFlag);
+    ReplayIntroVid();
   });
   var setActiveVidAndPlay = function(vidFlag2) {
+    clearTimeout(newTimer);
     allContentWrappers.forEach(function(el) {
       el.classList.remove("active");
+      if (vidFlag2 === "rotation") {
+        el.classList.add("active");
+      }
     });
-    document.querySelector(`.vid-${vidFlag2}`).currentTime = 0;
-    allVidWrappers.forEach(function(el) {
+    allVids.forEach(function(el) {
+      el.currentTime = 0;
+    });
+    allVidEndWrappers.forEach(function(el) {
+      el.classList.remove("active");
+    });
+    allVidAllWrappers.forEach(function(el) {
       el.classList.remove("active");
       if (el.classList.contains(vidFlag2)) {
         el.classList.add("active");
       }
     });
-    document.querySelector(`.vid-${vidFlag2}`).play();
+    allVidWrappers.forEach(function(el) {
+      el.classList.add("active");
+    });
+    document.querySelector(`.vid.${vidFlag2}`).play();
+  };
+  var setActiveEndVidAndPlay = function(vidFlag2) {
+    allVidWrappers.forEach(function(el) {
+      el.classList.remove("active");
+    });
+    allVidEndWrappers.forEach(function(el) {
+      el.classList.remove("active");
+      if (el.classList.contains(vidFlag2)) {
+        el.classList.add("active");
+      }
+      document.querySelector(`.vid.end.${vidFlag2}`).play();
+    });
+  };
+  var ReplayIntroVid = function() {
+    newTimer = setTimeout(() => {
+      setActiveVidAndPlay("rotation");
+    }, 3e3);
+  };
+  var mutationObserver = new MutationObserver((entries) => {
+    const target = entries[entries.length - 1].target.classList[1];
+    document.querySelector(`.btn-lottie.${target}`).click();
+    buttonContainer.style["pointer-events"] = "auto";
+  });
+  mutationObserver.observe(lottieCTRLSpin, { attributes: true });
+  mutationObserver.observe(lottieCTRLLights, { attributes: true });
+  mutationObserver.observe(lottieCTRLVanish, { attributes: true });
+  buttonContainer.addEventListener("click", function(e) {
+    const clicked = e.target.closest(".button-lottie");
+    if (!clicked) return;
+    lottieFlag = clicked.classList[1];
+    SetActiveLottie(`${lottieFlag}`);
+    document.querySelector(`.btn-lottie.${lottieFlag}`).click();
+  });
+  var SetActiveLottie = function(activeLottie) {
+    buttonContainer.style["pointer-events"] = "none";
+    allLotties.forEach((el) => {
+      el.classList.remove("active");
+      el.style.opacity = "0";
+      if (el.classList.contains(activeLottie)) {
+        el.classList.add("active");
+        el.style.opacity = "100";
+      }
+      allLottieContents.forEach((el2) => {
+        el2.classList.remove("active");
+        el2.style.opacity = "0";
+        if (el2.classList.contains(activeLottie)) {
+          el2.classList.add("active");
+        }
+      });
+    });
   };
 })();
--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// const lottieCTRLSpin = document.querySelector(".lottie-ctrl.spin");
-// const lottieCTRLLights = document.querySelector(".lottie-ctrl.lights");
-// const lottieCTRLVanish = document.querySelector(".lottie-ctrl.vanish");
-// const allLotties = document.querySelectorAll(".lottie-wrapper");
-// const allLottieContents = document.querySelectorAll(".lottie-content-wrapper");
-// const lottie1 = document.querySelector(".lottie-wrapper.first");
-// const lottieSpin = document.querySelector(".lottie-wrapper.spin");
-// const lottie2 = document.querySelector(".lottie-wrapper.second");
-// const lottie3 = document.querySelector(".lottie-wrapper.third");
-// const lottieThree = document.querySelector(".lottie-div.third");
-// const btnLottie = document.querySelector(".button-lottie");
-// const btn1 = document.querySelector(".btn-lottie.first");
-// const btn2 = document.querySelector(".btn-lottie.second");
-// const btn3 = document.querySelector(".btn-lottie.third");
-// const button1 = document.querySelector(".button-lottie.first");
-// const button2 = document.querySelector(".button-lottie.second");
-// const button3 = document.querySelector(".button-lottie.third");
-// const btnSpin = document.querySelector(".btn-lottie.spin");
-// const btnLights = document.querySelector(".btn-lottie.lights");
-// const btnVanish = document.querySelector(".btn-lottie.vanish");
-// const buttonContainer = document.querySelector(".btn-container");
-// let lottieFlag;
+const lottieCTRLSpin = document.querySelector(".lottie-ctrl.spin");
+const lottieCTRLLights = document.querySelector(".lottie-ctrl.lights");
+const lottieCTRLVanish = document.querySelector(".lottie-ctrl.vanish");
+const allLotties = document.querySelectorAll(".lottie-wrapper");
+const allLottieContents = document.querySelectorAll(".lottie-content-wrapper");
+const lottie1 = document.querySelector(".lottie-wrapper.first");
+const lottieSpin = document.querySelector(".lottie-wrapper.spin");
+const lottie2 = document.querySelector(".lottie-wrapper.second");
+const lottie3 = document.querySelector(".lottie-wrapper.third");
+const lottieThree = document.querySelector(".lottie-div.third");
+const btnLottie = document.querySelector(".button-lottie");
+const btn1 = document.querySelector(".btn-lottie.first");
+const btn2 = document.querySelector(".btn-lottie.second");
+const btn3 = document.querySelector(".btn-lottie.third");
+const button1 = document.querySelector(".button-lottie.first");
+const button2 = document.querySelector(".button-lottie.second");
+const button3 = document.querySelector(".button-lottie.third");
+const btnSpin = document.querySelector(".btn-lottie.spin");
+const btnLights = document.querySelector(".btn-lottie.lights");
+const btnVanish = document.querySelector(".btn-lottie.vanish");
+const buttonContainer = document.querySelector(".btn-container");
+let lottieFlag;
 //...........................................................
 const vidRotation = document.querySelector(".vid-rotation");
 const vidAllWrapFirst = document.querySelector(".vid-all-wrapper.first");
@@ -32,119 +32,127 @@ const vidContentWrapSecond = document.querySelector(
 const vidContentWrapThird = document.querySelector(
   ".vid-content-wrapper.third"
 );
-const vidView1 = document.querySelector(".vid-first");
-const vidView2 = document.querySelector(".vid-second");
-const vidView3 = document.querySelector(".vid-third");
-const allVidWrappers = document.querySelectorAll(".vid-all-wrapper");
+const vidView1 = document.querySelector(".vid.first");
+const vidView2 = document.querySelector(".vid.second");
+const vidView3 = document.querySelector(".vid.third");
+const allVidAllWrappers = document.querySelectorAll(".vid-all-wrapper");
 const allContentWrappers = document.querySelectorAll(".vid-content-wrapper");
+const allVidWrappers = document.querySelectorAll(".vid-wrapper");
 const allVids = document.querySelectorAll(".vid");
+const allVidEndWrappers = document.querySelectorAll(".vid-end-wrapper");
 const vidBtnContainer = document.querySelector(".btn-vid-container");
 let vidFlag;
-//...........................................................
-console.log("Aug 25, 2025-3:49");
-
+let newTimer;
 //...........................................................
 //VIDEO
 vidBtnContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".button-vid");
   if (!clicked) return;
   vidFlag = clicked.classList[1];
-  console.log(vidFlag);
   setActiveVidAndPlay(vidFlag);
 });
 
 vidView1.addEventListener("ended", function () {
-  console.log("video 1 has ended");
   vidContentWrapFirst.classList.add("active");
+  setActiveEndVidAndPlay(vidFlag);
+  ReplayIntroVid();
 });
 
 vidView2.addEventListener("ended", function () {
-  console.log("video 2 has ended");
   vidContentWrapSecond.classList.add("active");
+  setActiveEndVidAndPlay(vidFlag);
+  ReplayIntroVid();
 });
 
 vidView3.addEventListener("ended", function () {
-  console.log("video 3 has ended");
   vidContentWrapThird.classList.add("active");
+  setActiveEndVidAndPlay(vidFlag);
+  ReplayIntroVid();
 });
 //...........................................................
 const setActiveVidAndPlay = function (vidFlag) {
+  clearTimeout(newTimer);
   allContentWrappers.forEach(function (el) {
     el.classList.remove("active");
+    if (vidFlag === "rotation") {
+      el.classList.add("active");
+    }
   });
-  document.querySelector(`.vid-${vidFlag}`).currentTime = 0;
-  // allVids.forEach(function (el) {
-  //   el.currentTime = 0;
-  // });
-  allVidWrappers.forEach(function (el) {
+
+  allVids.forEach(function (el) {
+    el.currentTime = 0;
+  });
+
+  allVidEndWrappers.forEach(function (el) {
+    el.classList.remove("active");
+  });
+
+  allVidAllWrappers.forEach(function (el) {
     el.classList.remove("active");
     if (el.classList.contains(vidFlag)) {
       el.classList.add("active");
     }
   });
-  document.querySelector(`.vid-${vidFlag}`).play();
+  allVidWrappers.forEach(function (el) {
+    el.classList.add("active");
+  });
+  document.querySelector(`.vid.${vidFlag}`).play();
 };
 //...........................................................
+const setActiveEndVidAndPlay = function (vidFlag) {
+  allVidWrappers.forEach(function (el) {
+    el.classList.remove("active");
+  });
+  allVidEndWrappers.forEach(function (el) {
+    el.classList.remove("active");
+    if (el.classList.contains(vidFlag)) {
+      el.classList.add("active");
+    }
+    document.querySelector(`.vid.end.${vidFlag}`).play();
+  });
+};
 //...........................................................
-//LOTTIE
-// const mutationObserver = new MutationObserver((entries) => {
-//   const target = entries[entries.length - 1].target.classList[1];
-//   document.querySelector(`.btn-lottie.${target}`).click();
-//   buttonContainer.style["pointer-events"] = "auto";
-// });
-
-// mutationObserver.observe(lottieCTRLSpin, { attributes: true });
-// mutationObserver.observe(lottieCTRLLights, { attributes: true });
-// mutationObserver.observe(lottieCTRLVanish, { attributes: true });
-
-// buttonContainer.addEventListener("click", function (e) {
-//   const clicked = e.target.closest(".button-lottie");
-//   if (!clicked) return;
-//   lottieFlag = clicked.classList[1];
-//   SetActiveLottie(`${lottieFlag}`);
-//   document.querySelector(`.btn-lottie.${lottieFlag}`).click();
-// });
-// //...........................................................
-// const SetActiveLottie = function (activeLottie) {
-//   buttonContainer.style["pointer-events"] = "none";
-//   allLotties.forEach((el) => {
-//     el.classList.remove("active");
-//     el.style.opacity = "0";
-//     if (el.classList.contains(activeLottie)) {
-//       el.classList.add("active");
-//       el.style.opacity = "100";
-//     }
-//     allLottieContents.forEach((el) => {
-//       el.classList.remove("active");
-//       el.style.opacity = "0";
-//       if (el.classList.contains(activeLottie)) {
-//         el.classList.add("active");
-//       }
-//     });
-//   });
-// };
+const ReplayIntroVid = function () {
+  newTimer = setTimeout(() => {
+    setActiveVidAndPlay("rotation");
+  }, 3000);
+};
 //...........................................................
+// LOTTIE;
+const mutationObserver = new MutationObserver((entries) => {
+  const target = entries[entries.length - 1].target.classList[1];
+  document.querySelector(`.btn-lottie.${target}`).click();
+  buttonContainer.style["pointer-events"] = "auto";
+});
 
-//<style>
-// 	#vid{
+mutationObserver.observe(lottieCTRLSpin, { attributes: true });
+mutationObserver.observe(lottieCTRLLights, { attributes: true });
+mutationObserver.observe(lottieCTRLVanish, { attributes: true });
 
-//       @media (min-width: 1200px) {
-//         .my-element {
-//             width: 960px; /* Fixed width for desktops */
-//         }
-//     }
-//       @media (min-width: 768px) {
-//         .my-element {
-//             width: 700px; /* Fixed width for tablets */
-//         }
-//     }
-// </style>
-
-// //<video id= "dpVideo" muted= "muted" loop  width= 100%>
-// <source src= "https://uploads-ssl.webflow.com/6361575c831c26646b85abba/636834af43f4586344d2a95f_Crane Video - Display Panel-transcode.mp4">
-// </video>
-
-// video = document.querySelector("video")
-// video.playbackRate = 2
-
+buttonContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".button-lottie");
+  if (!clicked) return;
+  lottieFlag = clicked.classList[1];
+  SetActiveLottie(`${lottieFlag}`);
+  document.querySelector(`.btn-lottie.${lottieFlag}`).click();
+});
+//...........................................................
+const SetActiveLottie = function (activeLottie) {
+  buttonContainer.style["pointer-events"] = "none";
+  allLotties.forEach((el) => {
+    el.classList.remove("active");
+    el.style.opacity = "0";
+    if (el.classList.contains(activeLottie)) {
+      el.classList.add("active");
+      el.style.opacity = "100";
+    }
+    allLottieContents.forEach((el) => {
+      el.classList.remove("active");
+      el.style.opacity = "0";
+      if (el.classList.contains(activeLottie)) {
+        el.classList.add("active");
+      }
+    });
+  });
+};
 //...........................................................
